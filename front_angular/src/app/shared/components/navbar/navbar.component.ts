f91cf95eb12f3e44ac7ca6241f6025da87a86146
arturillo40 <arturojimenezalmontaras@gmail.com>
2025-01-17 13:31:26 +0100

    agregar componentes y estilos para las páginas de inicio, tienda, clase, eventos, contactos y registro; actualizar navbar y pruebas unitarias

--- front_angular/src/app/shared/components/navbar/navbar.component.ts
+++ front_angular/src/app/shared/components/navbar/navbar.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { routes } from '../../../app.routes';
 
 @Component({
   selector: 'shared-navbar',
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrl: './navbar.component.css',
 })
 export class NavbarComponent {
+  public menuItems = routes.filter((route) => route.path !== '**' && route.path !== 'login' && route.path !== 'register');
 
+  constructor() {}
 }
