7e8fb2ed6880bc8979b4432e22d144398a186731
arturillo40 <arturojimenezalmontaras@gmail.com>
2025-01-30 14:01:18 +0100

    pruebas deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
+  output: "static", // Necesario para GitHub Pages
+  base: "/Proyecto-final-tema-1-figma/",
   vite: {
     resolve: {
       alias: {
